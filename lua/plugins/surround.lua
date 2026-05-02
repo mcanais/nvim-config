b44eb34340916ea448dea2a2435da468b38e744e
Miguel Canais <miguelmariacanais@gmail.com>
2026-05-03 00:23:12 +0100

    Fixed surround plugin

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -5,20 +5,6 @@ return {
         surround = require("nvim-surround")
 
         surround.setup({
-            keymaps = {
-                insert = nil,
-                insert_line = nil,
-                normal = "<Leader>s",
-                normal_cur = "<Leader>ss",
-                normal_line = nil,
-                normal_cur_line = nil,
-                visual = "<Leader>s",
-                visual_line = nil,
-                delete = "ds",
-                change = "cs",
-                change_line = nil,
-            },    
-
             surrounds = {
                 ["("] = {
                     add = { "(", ")" },
--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -11,6 +11,5 @@ return {
 
 		vim.keymap.set("n", "zR", ufo.openAllFolds)
 		vim.keymap.set("n", "zM", ufo.closeAllFolds)
-		vim.keymap.set("n", "<Leader>z", "za")
 	end
 }
